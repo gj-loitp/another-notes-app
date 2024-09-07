d57c3fdbd5148a172b5bf6bf17d5e2a8f3f26a52
roy <loitp@galaxyjoy.vn>
2024-09-07 13:01:44 +0700

    update

--- app/src/main/kotlin/com/mckimquyen/notes/ui/sort/SortDialog.kt
+++ app/src/main/kotlin/com/mckimquyen/notes/ui/sort/SortDialog.kt
@@ -4,7 +4,6 @@ import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.maltaisn.notes.debugCheck
 import com.mckimquyen.notes.RApp
 import com.mckimquyen.notes.R
 import com.mckimquyen.notes.databinding.DlgSortBinding
@@ -15,6 +14,7 @@ import com.mckimquyen.notes.ui.SharedViewModel
 import com.mckimquyen.notes.ui.navGraphViewModel
 import com.mckimquyen.notes.ui.observeEvent
 import com.mckimquyen.notes.ui.viewModel
+import debugCheck
 import javax.inject.Inject
 import javax.inject.Provider
 
